033bbbc6271750dcdac1133714630bb54d28a84a
sajad mohamed <92307480+sajadmohamed@users.noreply.github.com>
2022-03-03 16:35:43 +0300

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+for (var i=0; i <= 100; i++){
+             if (i % 3 == 0 && i %5 ==0 ){
+                console.log('fizzbuzz',i)
+            }
+            else if (i % 3 == 0){
+                console.log('fizz',i)
+            }
+            else if (i %5 ==0){
+                console.log('Buzz',i) 
+            }
+            
+            
+        }
